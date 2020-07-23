34081a87989bcd92a8e6b4dbbd9c3f1d2678aa77
Daniel Chalk <dchalk@adaptavist.com>
2020-07-23 14:44:43 +0100

    fix: moved isolated rules into resources to custom rules can be added

--- subnets_isolated.tf
+++ subnets_isolated.tf
@@ -12,52 +12,66 @@ resource "aws_network_acl" "isolated" {
   vpc_id     = aws_vpc.this.id
   subnet_ids = aws_subnet.isolated.*.id
   tags       = module.labels.tags
+}
 
-  dynamic "ingress" {
-    for_each = aws_subnet.private.*.cidr_block
-    content {
-      protocol   = -1
-      rule_no    = 100 + ingress.key
-      action     = "allow"
-      cidr_block = ingress.value
-      from_port  = 0
-      to_port    = 0
-    }
-  }
+// KEEP PUBLIC OUT
 
-  dynamic "ingress" {
-    for_each = var.enable_ipv6 ? aws_subnet.private.*.ipv6_cidr_block : []
-    content {
-      protocol        = -1
-      rule_no         = 200 + ingress.key
-      action          = "allow"
-      ipv6_cidr_block = ingress.value
-      from_port       = 0
-      to_port         = 0
-    }
-  }
+resource "aws_network_acl_rule" "isolated_public_ingress" {
+  count          = length(aws_subnet.public)
+  network_acl_id = aws_network_acl.isolated.id
+  rule_action    = "deny"
+  rule_number    = 100 + count.index
+  protocol       = -1
+  cidr_block     = aws_subnet.public[count.index].cidr_block
+}
 
-  dynamic "egress" {
-    for_each = aws_subnet.private.*.cidr_block
-    content {
-      protocol   = -1
-      rule_no    = 100 + egress.key
-      action     = "allow"
-      cidr_block = egress.value
-      from_port  = 0
-      to_port    = 0
-    }
-  }
+resource "aws_network_acl_rule" "isolated_public_ingress_ip6" {
+  count           = length(aws_subnet.private)
+  network_acl_id  = aws_network_acl.isolated.id
+  rule_action     = "deny"
+  rule_number     = 110 + count.index
+  protocol        = -1
+  ipv6_cidr_block = aws_subnet.private[count.index].ipv6_cidr_block
+}
 
-  dynamic "egress" {
-    for_each = var.enable_ipv6 ? aws_subnet.private.*.ipv6_cidr_block : []
-    content {
-      protocol        = -1
-      rule_no         = 200 + egress.key
-      action          = "allow"
-      ipv6_cidr_block = egress.value
-      from_port       = 0
-      to_port         = 0
-    }
-  }
+//ALLOW PRIVATE IN
+
+resource "aws_network_acl_rule" "isolated_private_ingress" {
+  count          = length(aws_subnet.private)
+  network_acl_id = aws_network_acl.isolated.id
+  rule_action    = "allow"
+  rule_number    = 120 + count.index
+  protocol       = -1
+  cidr_block     = aws_subnet.private[count.index].cidr_block
+}
+
+resource "aws_network_acl_rule" "isolated_private_ingress_ip6" {
+  count           = length(aws_subnet.private)
+  network_acl_id  = aws_network_acl.isolated.id
+  rule_action     = "allow"
+  rule_number     = 130 + count.index
+  protocol        = -1
+  ipv6_cidr_block = aws_subnet.private[count.index].ipv6_cidr_block
+}
+
+// ALLOW PRIVATE OUT
+
+resource "aws_network_acl_rule" "isolated_private_egress" {
+  count          = length(aws_subnet.private)
+  network_acl_id = aws_network_acl.isolated.id
+  egress         = true
+  rule_action    = "allow"
+  rule_number    = 140 + count.index
+  protocol       = -1
+  cidr_block     = aws_subnet.private[count.index].cidr_block
+}
+
+resource "aws_network_acl_rule" "isolated_private_egress_ip6" {
+  count           = length(aws_subnet.private)
+  network_acl_id  = aws_network_acl.isolated.id
+  egress          = true
+  rule_action     = "allow"
+  rule_number     = 150 + count.index
+  protocol        = -1
+  ipv6_cidr_block = aws_subnet.private[count.index].ipv6_cidr_block
 }
